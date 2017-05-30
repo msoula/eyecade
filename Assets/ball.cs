871f2dbdd77d12e2a59ad4e73d2851032e59ac35
BeaumerF <tit_shadow_shao@hotmail.fr>
2017-05-31 00:24:06 +0200

    initial import

---
+++ Assets/ball.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ball : MonoBehaviour {
+
+    public float speed = 30;
+
+	void Start () {
+        GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
+	}
+
+    float HitFactor(Vector2 Ball_Pos, Vector2 Racket_Pos, float Racket_Height) {
+        return ((Ball_Pos.y - Racket_Pos.y) / Racket_Height);
+    }
+
+    void OnCollisionEnter2D(Collision2D col) {
+        if (col.gameObject.name == "Racket_left")
+        {
+            float y = HitFactor(transform.position, col.transform.position, col.collider.bounds.size.y);
+            Vector2 dir = new Vector2(1, y).normalized;
+            GetComponent<Rigidbody2D>().velocity = dir * speed;
+            GetComponent<TrailRenderer>().materials[0].SetColor("_EmissionColor", Color.blue);
+        }
+        else if (col.gameObject.name == "Racket_right")
+        {
+            float y = HitFactor(transform.position, col.transform.position, col.collider.bounds.size.y);
+            Vector2 dir = new Vector2(-1, y).normalized;
+            GetComponent<Rigidbody2D>().velocity = dir * speed;
+            GetComponent<TrailRenderer>().materials[0].SetColor("_EmissionColor", Color.red);
+        }
+    }
+}
---
+++ Assets/move_racket.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move_racket : MonoBehaviour {
+
+    public float speed = 30;
+    public string axis = "Vertical";
+
+    void FixedUpdate () {
+        float v = Input.GetAxisRaw(axis);
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0, v) * speed;
+	}
+}
