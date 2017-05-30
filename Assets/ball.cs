d4e8bf473bfb9361e7407c0a3db93f6fc4d38a5f
rid <msoula@gmx.com>
2017-05-31 01:37:06 +0200

    Rename assets (uniformity) and reset ball when wall touched.

--- Assets/ball.cs
+++ Assets/ball.cs
@@ -15,19 +15,30 @@ public class ball : MonoBehaviour {
     }
 
     void OnCollisionEnter2D(Collision2D col) {
-        if (col.gameObject.name == "Racket_left")
+        if (col.gameObject.name == "racket_left")
         {
             float y = HitFactor(transform.position, col.transform.position, col.collider.bounds.size.y);
             Vector2 dir = new Vector2(1, y).normalized;
             GetComponent<Rigidbody2D>().velocity = dir * speed;
             GetComponent<TrailRenderer>().materials[0].SetColor("_EmissionColor", Color.blue);
         }
-        else if (col.gameObject.name == "Racket_right")
+        if (col.gameObject.name == "racket_right")
         {
             float y = HitFactor(transform.position, col.transform.position, col.collider.bounds.size.y);
             Vector2 dir = new Vector2(-1, y).normalized;
             GetComponent<Rigidbody2D>().velocity = dir * speed;
             GetComponent<TrailRenderer>().materials[0].SetColor("_EmissionColor", Color.red);
         }
+        if (col.gameObject.name == "wall_right") {
+            GetComponent<Rigidbody2D>().position = new Vector3(0, 0, 0);
+            GetComponent<Rigidbody2D>().velocity = Vector2.left * speed;
+            GetComponent<TrailRenderer>().Clear();
+        }
+        if (col.gameObject.name == "wall_left") {
+            GetComponent<Rigidbody2D>().position = new Vector3(0, 0, 0);
+            GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
+            GetComponent<TrailRenderer>().Clear();
+        }
+
     }
 }
