ea590faed1e9196f392c05c5bc2c622aca501300
rid <msoula@gmx.com>
2017-05-31 18:46:39 +0200

    Add arkanoid scene.

---
+++ Assets/Scripts/Arkanoid/Ball.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Arkanoid {
+
+public class Ball : MonoBehaviour {
+
+    public float speed = 100f;
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Rigidbody2D>().velocity = Vector2.up * speed;
+	}
+
+	// Update is called once per frame
+	void Update () {
+    }
+
+	float hitFactor(Vector2 ballPos, Vector2 racketPos, float racketWidth) {
+        // ascii art:
+        //
+        // 1  -0.5  0  0.5   1  <- x value
+        // ===================  <- racket
+        //
+        return (ballPos.x - racketPos.x) / racketWidth;
+    }
+
+    void OnCollisionEnter2D(Collision2D col) {
+        // Hit the Racket?
+        if (col.gameObject.name == "racket") {
+            // Calculate hit Factor
+            float x=hitFactor(transform.position, col.transform.position, col.collider.bounds.size.x);
+
+            // Calculate direction, set length to 1
+            Vector2 dir = new Vector2(x, 1).normalized;
+
+            // Set Velocity with dir * speed
+            GetComponent<Rigidbody2D>().velocity = dir * speed;
+        }
+    }
+}
+
+}
---
+++ Assets/Scripts/Arkanoid/Racket.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Arkanoid {
+
+public class Racket : MonoBehaviour {
+
+    public float speed = 150;
+
+	// Update is called once per frame
+	void FixedUpdate () {
+        float h = Input.GetAxisRaw("Horizontal");
+        GetComponent<Rigidbody2D>().velocity = Vector2.right * h * speed;
+	}
+}
+
+}
