95fca68bcc413b71c16b811704de3159970db9b4
rid <msoula@gmx.com>
2017-06-01 03:27:37 +0200

    Add score to arkanoid.

--- Assets/Scripts/Arkanoid/Block.cs
+++ Assets/Scripts/Arkanoid/Block.cs
@@ -6,9 +6,20 @@ namespace Arkanoid {
 
 public class Block : MonoBehaviour {
 
+    public float gain = 1;
     public string color = "Blue";
 
+    private Score score;
+
+    void Start() {
+        GameObject scoreObj = GameObject.Find("score");
+        score = scoreObj.GetComponent<Score>();
+    }
+
     void OnCollisionEnter2D(Collision2D collisionInfo) {
+
+        score.OnScoreInc(gain);
+
         gameObject.GetComponent<Animator>().SetTrigger(color);
     }
 }
---
+++ Assets/Scripts/Arkanoid/Score.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour {
+
+    private UnityEngine.UI.Text text;
+    private float score = 0;
+
+	// Use this for initialization
+	void Start () {
+        text = GetComponent<UnityEngine.UI.Text>();
+        Reset();
+	}
+
+    public void Reset() {
+        score = 0;
+    }
+
+    public void OnScoreInc(float inc) {
+        score += inc;
+    }
+
+    public void OnScoreDec(float inc) {
+        score -= inc;
+        score = Mathf.Max(score, 0);
+    }
+
+	// Update is called once per frame
+	void Update () {
+        text.text = score.ToString("00000000");
+	}
+}
